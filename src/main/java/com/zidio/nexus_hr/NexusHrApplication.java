f802b6c76b34a14c67e09162abff81bae9ae0f42
Daniel Ochigbo <ochigbodaniel240@gmail.com>
2026-06-12 09:05:39 +0100

    refactor: create project folders and structure

---
+++ src/main/java/com/zidio/nexus_hr/NexusHrApplication.java
@@ -0,0 +1,13 @@
+package com.zidio.nexus_hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusHrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zidio/nexus_hr/NexusHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zidio.nexus_hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
