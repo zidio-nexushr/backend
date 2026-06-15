1b571f749d032dff925eedf17756df40562048d3
snehaa147 <snehashingare2018@gmail.com>
2026-06-15 16:23:54 +0530

    feat: add user entity and repository

---
+++ src/main/java/com/zidio/nexus_hr/authservice/entity/User.java
@@ -0,0 +1,32 @@
+package com.zidio.nexus_hr.authservice.entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
